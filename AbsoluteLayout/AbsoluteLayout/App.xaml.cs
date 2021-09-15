a3a61e8685cd0f13c7cfc24c93e62d2bec983f2d
Gilbert Enriquez <gilbertenriquez10@gmail.com>
2021-09-15 11:35:30 +0800

    Add project files.

---
+++ AbsoluteLayout/AbsoluteLayout/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AbsoluteLayout
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AbsoluteLayout2();
+            //same for stacklayout and gridlayout just change the mainpage to run exercise 2/Absolutelayout2
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
